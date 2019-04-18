726c2812c1eadde17ac3c77edd287b66f2f54c0f
Christiansantoss <nismoskyi@yahoo.com>
2019-04-18 15:37:28 -0700

    added notes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  # routes have "priority". First come first serve.
+  # you can check all of routes with 'rake routes'
   # set the index page / root url
   
   root 'pages#home'
@@ -10,3 +13,33 @@ Rails.application.routes.draw do
   get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+
+  # root 'welcome#index'
+  
+  # Example of regular route: 
+  # get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  # get 'products/id:id/purchase' => 'catalog#purchase' ,as: :purchase
+
+  # Example resource route   (maps HTTP verbs to controller actions automatically):
+  # resources :products
+
+  # Example resource route with options:
+  # resources :products do
+  #   member do 
+  #     get 'short'
+  #      post 'toggle'
+  #   end
+  #
+  #   collection do
+  #     get 'sold'
+  #   end
+  # end
+
+  
