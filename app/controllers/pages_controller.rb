36ae84a618633791d8aa3fdcb640eaafcefce656
Christiansantoss <nismoskyi@yahoo.com>
2019-04-19 00:46:06 -0700

    added instance for time

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,11 @@ class PagesController < ApplicationController
   end
 
   def home
+    @time = Time.now # change our action method in the controller to set the same time value into an instance variable called @time.
   end
-
+  
   def contact
   end
 end
+
+# when we refresh the browser we see the current time again showing that the communication between the controller and the view was successful.
